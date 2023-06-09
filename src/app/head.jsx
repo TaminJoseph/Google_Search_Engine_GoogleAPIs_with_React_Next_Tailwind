faed426a9f3dcb1802e2bbadceea5fa6b5b6becd
idrismuhd <idrismuhd41@gmail.com>
2023-06-09 18:06:00 +0100

    First commit

---
+++ src/app/head.jsx
@@ -0,0 +1,13 @@
+export default function Head() {
+  return (
+    <>
+      <title>Google Engine Clone</title>
+      <meta content='width=device-width, initial-scale=1' name='viewport' />
+      <meta
+        name='description'
+        content='Google Engine Clone created by Latest Next js'
+      />
+      <link rel='icon' href='/favicon.svg' />
+    </>
+  );
+}
---
+++ src/app/layout.js
@@ -0,0 +1,15 @@
+import Footer from '@/components/Footer';
+import './globals.css';
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang='en'>
+      <body className='relative min-h-screen'>
+        {children}
+        {/* Footer */}
+
+        <Footer />
+      </body>
+    </html>
+  );
+}
